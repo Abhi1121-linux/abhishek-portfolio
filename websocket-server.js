4c394da32e1fe020b20de8e2de0970c41c417034
Abhi1121-linux <abhi112107@gmail.com>
2025-11-07 13:42:17 +0530

    Initial modern portfolio with WebSocket

--- websocket-server.js
+++ websocket-server.js
@@ -1,17 +1,8 @@
 const WebSocket = require('ws');
-const fs = require('fs');
-const path = require('path');
-
 const wss = new WebSocket.Server({ port: 3000 });
 
 wss.on('connection', ws => {
   console.log('Client connected');
-  ws.send('Connected to WebSocket');
-
-  // Watch for changes in website folder
-  const watchDir = path.join(__dirname);
-  fs.watch(watchDir, { recursive: true }, (eventType, filename) => {
-    ws.send(`Update detected: ${filename}`);
-  });
+  ws.send('Connected to WebSocket server');
 });
 
